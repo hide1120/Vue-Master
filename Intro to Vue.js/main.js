58810aabe8d81feec0cc4d75a96f485f072fc1d2
hide1120 <takahide1120@gmail.com>
2020-07-22 16:42:09 +0900

    ch03

--- Intro to Vue.js/main.js
+++ Intro to Vue.js/main.js
@@ -2,6 +2,9 @@ var app = new Vue({
   el: "#app",
   data: {
     product: "Socks",
-    image: ""
+    image: "assets/green-sox.jpg",
+    fluid: "img-fluid",
+    //inStock: true
+    inventory: 5
   }
 })
